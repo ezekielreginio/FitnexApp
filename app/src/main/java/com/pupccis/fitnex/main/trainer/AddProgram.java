c760e411fd5ef715c584afde4c4b4ae8e85ecb86
reddhilario <84271181+reddhilario@users.noreply.github.com>
2021-08-21 04:11:36 +0800

    add program transparent

--- app/src/main/java/com/pupccis/fitnex/main/trainer/AddProgram.java
+++ app/src/main/java/com/pupccis/fitnex/main/trainer/AddProgram.java
@@ -3,15 +3,32 @@ package com.pupccis.fitnex.main.trainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.pupccis.fitnex.R;
 
 public class AddProgram extends AppCompatActivity {
-
+    Animation rotateAnimation;
+    ImageView imageView;
+    RelativeLayout closeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_program);
+        imageView = (ImageView) findViewById(R.id.closeAddProgramButton);
+        closeButton = (RelativeLayout) findViewById(R.id.relativeLayoutAddProgramCloseButton);
+
+        rotateAnimation();
+        closeButton.setVisibility(View.VISIBLE);
+    }
+
+    private void rotateAnimation() {
+        rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        imageView.startAnimation(rotateAnimation);
     }
 
 }
--- app/src/main/java/com/pupccis/fitnex/main/trainer/TrainerDashboard.java
+++ app/src/main/java/com/pupccis/fitnex/main/trainer/TrainerDashboard.java
@@ -1,29 +1,35 @@
 package com.pupccis.fitnex.main.trainer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.pupccis.fitnex.API.adapter.FragmentAdapter;
 import com.pupccis.fitnex.R;
 import com.pupccis.fitnex.login.FitnexLogin;
+import com.pupccis.fitnex.login.FitnexRegister;
 
 public class TrainerDashboard extends AppCompatActivity implements View.OnClickListener{
 
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     FragmentAdapter fragmentAdapter;
-    LinearLayout programPanel;
-    ImageView addButton;
+    LinearLayout programPanel, addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +37,7 @@ public class TrainerDashboard extends AppCompatActivity implements View.OnClickL
 
         tabLayout = findViewById(R.id.tabLayoutTrainerDashboard);
         viewPager2 = findViewById(R.id.viewPager2TrainerDashboard);
-        addButton = (ImageView) findViewById(R.id.AddProgramButton);
+        addButton = (LinearLayout) findViewById(R.id.linearLayoutAddProgramButton);
         programPanel = (LinearLayout) findViewById(R.id.linearLayoutTrainerDashboardNavbar);
         addButton.setOnClickListener(this);
         FragmentManager fm = getSupportFragmentManager();
@@ -71,7 +77,13 @@ public class TrainerDashboard extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            case R.id.AddProgramButton:
+            case R.id.linearLayoutAddProgramButton:
+//                Intent intent = new Intent(TrainerDashboard.this, AddProgram.class);
+//                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(TrainerDashboard.this, programPanel, ViewCompat.getTransitionName(programPanel));
+//                startActivity(intent, optionsCompat.toBundle());
+//                addButton.setVisibility(View.GONE);
+                startActivity(new Intent(this, AddProgram.class));
+                overridePendingTransition(R.anim.slide_in_bottom,R.anim.stay);
                 Toast.makeText(TrainerDashboard.this, "Click working ", Toast.LENGTH_SHORT).show();
 
         }
