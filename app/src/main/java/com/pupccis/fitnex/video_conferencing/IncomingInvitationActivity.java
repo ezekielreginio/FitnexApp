ea6d901fa40f0614e29cb93ee9d8bbd96c8227b2
ezekielreginio <78013755+ezekielreginio@users.noreply.github.com>
2021-08-21 04:10:39 +0800

    save video confereencing

--- app/src/main/java/com/pupccis/fitnex/video_conferencing/IncomingInvitationActivity.java
+++ app/src/main/java/com/pupccis/fitnex/video_conferencing/IncomingInvitationActivity.java
@@ -1,13 +1,21 @@
 package com.pupccis.fitnex.video_conferencing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pupccis.fitnex.R;
 import com.pupccis.fitnex.utilities.Constants;
+import com.pupccis.fitnex.video_conferencing.network.ApiClient;
+import com.pupccis.fitnex.video_conferencing.network.ApiService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class IncomingInvitationActivity extends AppCompatActivity {
 
@@ -31,5 +39,28 @@ public class IncomingInvitationActivity extends AppCompatActivity {
 
         textUsername.setText(getIntent().getStringExtra(Constants.KEY_FULLNAME));
         textEmail.setText(getIntent().getStringExtra(Constants.KEY_EMAIL));
+    }
+
+
+
+    private void sendRemoteMessage(String remoteMessageBody, String type){
+        ApiClient.getClient().create(ApiService.class).sendRemoteMessage(
+                Constants.getRemoteMessageHeaders(), remoteMessageBody
+        ).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, Response<String> response) {
+                if(response.isSuccessful()){
+                }
+                else{
+                    Toast.makeText(IncomingInvitationActivity.this, response.message(),Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+                Toast.makeText(OutgoingInvitationActivity.this, t.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
