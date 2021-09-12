729b880232c3481201ced2216ddeeafc17749183
reddhilario <84271181+reddhilario@users.noreply.github.com>
2021-09-13 05:04:19 +0800

    with routine page design

---
+++ app/src/main/java/com/pupccis/fitnex/Activities/Routine/RoutinePage.java
@@ -0,0 +1,16 @@
+package com.pupccis.fitnex.Activities.Routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pupccis.fitnex.R;
+
+public class RoutinePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine_page);
+    }
+}
