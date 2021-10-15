f77e8717c9fc6149005289ca536644cc3a0b04eb
reddhilario <84271181+reddhilario@users.noreply.github.com>
2021-10-16 01:32:57 +0800

    ... 132

---
+++ app/src/main/java/com/pupccis/fitnex/activities/nutritiontracking/DietPage.java
@@ -0,0 +1,16 @@
+package com.pupccis.fitnex.activities.nutritiontracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pupccis.fitnex.R;
+
+public class DietPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_page);
+    }
+}
